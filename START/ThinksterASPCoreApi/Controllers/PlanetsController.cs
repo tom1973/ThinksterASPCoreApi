9c435ea488ffeec5f607273157cf353acc0be663
tom1973 <toomas.juhkov@itcollege.ee>
2022-09-14 16:09:58 +0300

    initial commit

---
+++ START/ThinksterASPCoreApi/Controllers/PlanetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThinksterASPCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlanetsController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new { label = "value1", label2 = "value2" };
+        }
+    }
+}
